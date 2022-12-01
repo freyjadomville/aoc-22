40f2bfc149031f784cd78a6819b868de34611fd4
Freyja Domville <freyja.domville@67bricks.com>
2022-12-01 18:33:18 +0000

    push to existing vec instead of cloning and returning values

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 fn main() {
     let input = include_str!("../puzzle-input.txt");
-    let mut elf_totals = input.lines().fold(vec![], |elves:Vec<u32>, new_value| {
-        if elves.len() == 0 { vec![new_value.parse().expect("First value wasn't a u32")]}
-        else if new_value == "" { [elves.as_slice(), [0].as_slice()].concat() }
+    let mut elf_totals = input.lines().fold(vec![], |mut elves:Vec<u32>, new_value| {
+        if elves.len() == 0 { elves.push(new_value.parse().expect("First value wasn't a u32"));}
+        else if new_value == "" { elves.push(0) }
         else { 
-            let mut new_vec = elves.clone(); 
-            let last = new_vec.last_mut().unwrap(); 
+            let last = elves.last_mut().unwrap(); 
             *last = *last + new_value.parse::<u32>().expect("Value wasn't a u32");
-            new_vec
         }
+        elves
     });
     // Descending order
     elf_totals.sort_by(|a,b| b.cmp(a));
