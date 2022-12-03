3304038bb9925227bab4ec2353e9afe57cebfd3f
Freyja Domville <freyjadomville@gmail.com>
2022-12-03 16:37:04 +0000

    expect instead of unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 fn main() {
     let input = include_str!("../puzzle-input.txt");
-    let mut elf_totals = input
-        .lines()
-        .fold(vec![], |mut elves: Vec<u32>, new_value| {
-            if elves.len() == 0 {
-                elves.push(new_value.parse().expect("First value wasn't a u32"));
-            } else if new_value == "" {
-                elves.push(0)
-            } else {
-                let last = elves.last_mut().unwrap();
-                *last = *last + new_value.parse::<u32>().expect("Value wasn't a u32");
-            }
-            elves
-        });
+    let mut elf_totals = input.lines().fold(vec![], |mut elves, new_value| {
+        if elves.len() == 0 {
+            elves.push(new_value.parse().expect("First value wasn't a u32"));
+        } else if new_value == "" {
+            elves.push(0)
+        } else {
+            let last = elves.last_mut().expect("no 'last element' to access");
+            *last = *last + new_value.parse::<u32>().expect("Value wasn't a u32");
+        }
+        elves
+    });
     // Descending order
     elf_totals.sort_by(|a, b| b.cmp(a));
 
