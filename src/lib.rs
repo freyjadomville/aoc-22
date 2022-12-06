2b4fbaf680630dd16a99323fec3ae3b034d6096e
Freyja Domville <freyjadomville@gmail.com>
2022-12-06 20:20:44 +0000

    Templte for Day 2 and beyond

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+pub mod aoc {
+    pub fn advent_one() {
+        let input = include_str!("../puzzle-input-01.txt");
+        let mut elf_totals = input.lines().fold(vec![], |mut elves, new_value| {
+            if elves.len() == 0 {
+                elves.push(new_value.parse().expect("First value wasn't a u32"));
+            } else if new_value == "" {
+                elves.push(0)
+            } else {
+                let last = elves.last_mut().expect("no 'last element' to access");
+                *last = *last + new_value.parse::<u32>().expect("Value wasn't a u32");
+            }
+            elves
+        });
+        // Descending order
+        elf_totals.sort_by(|a, b| b.cmp(a));
+
+        let part_1 = elf_totals.iter().max().expect("No result found");
+        let part_2: u32 = elf_totals.iter().take(3).sum();
+
+        println!("{}", part_1);
+        println!("{}", part_2);
+    }
+
+    pub fn advent_two() {
+        let input = include_str!("../puzzle-input-02.txt");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
+use aoc_2022::aoc;
+
 fn main() {
-    let input = include_str!("../puzzle-input.txt");
-    let mut elf_totals = input.lines().fold(vec![], |mut elves, new_value| {
-        if elves.len() == 0 {
-            elves.push(new_value.parse().expect("First value wasn't a u32"));
-        } else if new_value == "" {
-            elves.push(0)
-        } else {
-            let last = elves.last_mut().expect("no 'last element' to access");
-            *last = *last + new_value.parse::<u32>().expect("Value wasn't a u32");
-        }
-        elves
-    });
-    // Descending order
-    elf_totals.sort_by(|a, b| b.cmp(a));
+    let day = std::env::args().nth(1).expect("No day given");
 
-    let part_1 = elf_totals.iter().max().expect("No result found");
-    let part_2: u32 = elf_totals.iter().take(3).sum();
-
-    println!("{}", part_1);
-    println!("{}", part_2);
+    match day.as_str() {
+        "1" => aoc::advent_one(),
+        "2" => aoc::advent_two(),
+        _ => println!("Day not implemented"),
+    }
 }
