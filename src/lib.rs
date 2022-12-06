ec2fe16886bd0d3440d09c6c2f19ea17433fc79e
Freyja Domville <freyjadomville@gmail.com>
2022-12-06 20:49:56 +0000

    Day 2 solution

--- src/lib.rs
+++ src/lib.rs
@@ -24,5 +24,40 @@ pub mod aoc {
 
     pub fn advent_two() {
         let input = include_str!("../puzzle-input-02.txt");
+
+        let part_1 = input
+            .lines()
+            .map(|value| match value {
+                "A X" => 1 + 3,
+                "B X" => 1 + 0,
+                "C X" => 1 + 6,
+                "A Y" => 2 + 6,
+                "B Y" => 2 + 3,
+                "C Y" => 2 + 0,
+                "A Z" => 3 + 0,
+                "B Z" => 3 + 6,
+                "C Z" => 3 + 3,
+                _ => panic!("Unrecorgnised Value: {}", value),
+            })
+            .sum::<u64>();
+
+        let part_2 = input
+            .lines()
+            .map(|value| match value {
+                "A X" => 3 + 0,
+                "B X" => 1 + 0,
+                "C X" => 2 + 0,
+                "A Y" => 1 + 3,
+                "B Y" => 2 + 3,
+                "C Y" => 3 + 3,
+                "A Z" => 2 + 6,
+                "B Z" => 3 + 6,
+                "C Z" => 1 + 6,
+                _ => panic!("Unrecorgnised Value: {}", value),
+            })
+            .sum::<u64>();
+
+        println!("{}", part_1);
+        println!("{}", part_2);
     }
 }
